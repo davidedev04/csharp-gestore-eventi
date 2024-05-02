de7b721c83d606a01c604f355a6ce6d0bbbff787
Davide Prencipe <davidedev04@gmail.com>
2024-05-02 09:45:05 +0200

    Aggiungere i file di progetto.

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Evento
+    {
+        public string Titolo {  get; set; }
+        public DateTime Data {  get; set; }
+        public int Capienza {  get; set; }
+        public int PostiPrenotati {  get; set; }
+
+        public Evento() 
+        {
+            
+        }
+
+    }
+}
